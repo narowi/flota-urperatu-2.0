66ba393684b700fe527bbc0dec0e3e8cd73f7bd1
olatz18 <olatzurrutia@hotmail.es>
2017-05-05 16:53:50 +0200

    ikututa=false

--- sprint1/Kasilla.java
+++ sprint1/Kasilla.java
@@ -1,112 +1,112 @@
 package sprint1;
 
 public class Kasilla {
-	private boolean begiratuta=false;
-	//private boolean ezkutua=false;
-	private Ontzia ontzi;
-	private boolean ura=true;
-	private int x=0;
-	private int y=0;
-	private boolean urperatuta=false;
-	private boolean ikutuGabe=true;
-	//junit
-		private boolean ikututa=true;
-		
-	
-	//public void setEzkutua(boolean pEzkutua){
-		//ezkutua=pEzkutua;
-	//}
-	public Kasilla(int i, int j){
-		x = i;
-		y = j;
-	}
+	 private boolean begiratuta=false;
+	 //private boolean ezkutua=false;
+	 private Ontzia ontzi;
+	 private boolean ura=true;
+	 private int x=0;
+	 private int y=0;
+	 private boolean urperatuta=false;
+	 private boolean ikutuGabe=true;
+	 //junit
+	  private boolean ikututa=false;
+	  
+	 
+	 //public void setEzkutua(boolean pEzkutua){
+	  //ezkutua=pEzkutua;
+	 //}
+	 public Kasilla(int i, int j){
+	  x = i;
+	  y = j;
+	 }
 
-	public Ontzia getOntzia() {
-		return this.ontzi;
-	}
+	 public Ontzia getOntzia() {
+	  return this.ontzi;
+	 }
 
-	public boolean getUra() {
-		return this.ura; // si ura esta a true es que al lado hay un ontzi
-	}
-	
-	public void setUra(){
-		this.ura=false;
-	}
+	 public boolean getUra() {
+	  return this.ura; // si ura esta a true es que al lado hay un ontzi
+	 }
+	 
+	 public void setUra(){
+	  this.ura=false;
+	 }
 
-	public void ontziaJarri(Ontzia o) {
-		this.ontzi=o;
-	}
+	 public void ontziaJarri(Ontzia o) {
+	  this.ontzi=o;
+	 }
 
-	public void kenduUra() {
-		this.ura=false;
-	}
+	 public void kenduUra() {
+	  this.ura=false;
+	 }
 
-	public char getZerNahiz() {
-		if(ontzi!=null){
-			return 'U';
-		}
-		else{
-			return 'B';
-		}
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
+	 public char getZerNahiz() {
+	  if(ontzi!=null){
+	   return 'U';
+	  }
+	  else{
+	   return 'B';
+	  }
+	 }
+	 public int getX() {
+	  return x;
+	 }
+	 public int getY() {
+	  return y;
+	 }
 
-	public void setBegiratuta(boolean b) {
-		begiratuta=b;
-		
+	 public void setBegiratuta(boolean b) {
+	  begiratuta=b;
+	  
+	 }
+
+	 public boolean getBegiratuta() {
+	  return begiratuta;
+	 }
+
+	 //junit
+	// public int ikutuGabe() {
+	//  if(this.ikutuGabe){
+	//   return 1;
+	//  }else{
+	//   return 0;
+	//  }
+	//  
+	// }
+
+	// public void setIkutuGabe(boolean b) {
+	//  this.ikutuGabe=b;
+	//  
+	// }
+
+	 public void setUrperatuta(boolean b) {
+	  this.urperatuta=b;
+	  
+	 }
+
+	 public boolean getUrperatuta() {
+	  return this.urperatuta;
+	 }
+	 public void markatuIkututa(boolean b) {
+	  this.ikututa=b;
+	  
+	 }
+
+	 public boolean getIkututa() {
+	  return this.ikututa;
+	 }
+
+	 public int ikutuGabe() {
+	  if(this.ikutuGabe){
+	   return 1;
+	  }else{
+	   return 0;
+	  }
+	 }
+
+	 public void setIkutuGabe(boolean b) {
+	  this.ikutuGabe=b;
+	 }
+
 	}
-
-	public boolean getBegiratuta() {
-		return begiratuta;
-	}
-
-	//junit
-//	public int ikutuGabe() {
-//		if(this.ikutuGabe){
-//			return 1;
-//		}else{
-//			return 0;
-//		}
-//		
-//	}
-
-//	public void setIkutuGabe(boolean b) {
-//		this.ikutuGabe=b;
-//		
-//	}
-
-	public void setUrperatuta(boolean b) {
-		this.urperatuta=b;
-		
-	}
-
-	public boolean getUrperatuta() {
-		return this.urperatuta;
-	}
-	public void markatuIkututa(boolean b) {
-		this.ikututa=b;
-		
-	}
-
-	public boolean getIkututa() {
-		return this.ikututa;
-	}
-
-	public int ikutuGabe() {
-		if(this.ikutuGabe){
-			return 1;
-		}else{
-			return 0;
-		}
-	}
-
-	public void setIkutuGabe(boolean b) {
-		this.ikutuGabe=b;
-	}
-
-}
